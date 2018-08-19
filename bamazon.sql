a8302836de47db8cc73dfb85fa7fb6cd8eaf4732
Robiel <robielasgedom@gmail.com>
2018-08-19 00:12:07 -0400

    update

--- bamazon.sql
+++ bamazon.sql
@@ -10,3 +10,18 @@ CREATE TABLE products(
     stockquantity INTEGER(10) NOT NULL,
     PRIMARY KEY (itemid)    
 );
+
+INSERT INTO products(Product_Name, Department_Name, Price, Stock_Quantity)
+VALUES("Levis", "Apparel", 89.99, 11),
+("Safety razor", "Men's Grooming", 39.99, 15),
+("Rubik's Cube", "Puzzles/Games", 19.99, 7),
+("Bose Speaker", "Electronics", 219.99, 10),
+("Sunglasses", "Eyewear", 179.99, 15),
+("Table Fan", "Home", 14.99, 20),
+("Nike Shoes", "Footwear", 149.99, 5),
+("Raspberry Pi", "Electronics", 29.99, 22),
+("Pressure Cooker", "Kitchen", 74.99, 3),
+("Car Tires", "Automotive", 399.99, 4);
+
+
+SELECT * FROM bamazon_db.products;
